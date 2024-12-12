feaad3453db1acb5a7afab654ce0aaaee37ae98e
AurelDev <elipticgamepro@gmail.com>
2024-12-12 18:53:03 +0100

    Adding a FAQ page

---
+++ src/views/FAQ.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import App from '@/App.vue';
+    const { ApiURL, Language } = App.components;
+</script>
+
+<template>
+    <section>
+        <div>
+            
+        </div>
+    </section>
+</template>
+
+<style lang="css">
+
+</style>
